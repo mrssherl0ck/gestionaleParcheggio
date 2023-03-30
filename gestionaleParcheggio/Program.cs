38a37ef0e66827da580491c85327ea0cbe7820d0
corso <corso@INFO2ACER13>
2023-03-30 17:58:08 +0200

    Add project files.

---
+++ gestionaleParcheggio/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestionaleParcheggio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         //Strutte per contenere i dati
+           List<Veicolo> parcheggio = new List<Veicolo>();
+
+            while (true)
+            {
+                //Console.Clear();
+                Console.WriteLine("1- Inserire Veicolo");
+                Console.WriteLine("2- Stampa veicoli presenti");
+                Console.WriteLine("3- Uscita Veicolo");
+                Console.WriteLine("4- Tariffa Oraria 2 euro. Importo da pagare:");
+                Console.WriteLine("\n9- Esci");
+
+                string scelta = Console.ReadLine();
+
+
+
+                if (scelta == "1")
+                {
+                    Console.WriteLine("Per favore inserire Targa Veicolo");
+                    string targa = Console.ReadLine();
+                    parcheggio.Add(new Veicolo(targa));
+
+                }
+
+                if (scelta == "2")
+                {
+                    foreach (Veicolo v in parcheggio)
+                    {   
+                        Console.WriteLine(v);
+                    }
+                }
+
+                if (scelta == "3")
+                {
+                    Console.WriteLine("Per favore inserire Targa Veicolo");
+                    string ricerca = Console.ReadLine();
+                    foreach (Veicolo v in parcheggio)
+                    {
+                        parcheggio.RemoveAt(parcheggio.IndexOf(v));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ gestionaleParcheggio/Veicolo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestionaleParcheggio
+{
+    internal class Veicolo
+    {  //Campi
+        public string targa;
+        public string entrata;
+        public string uscita;
+
+        //Obiettivo lettura e scrittura
+        public DateTime dataOraEntrata { get; set; } = DateTime.Now;
+        public DateTime? dataOraUscita { get; set; } = null;
+
+        //Costruttori
+        public Veicolo(string targa)
+        { this.targa = targa; }
+        /*string entrata, string uscita, DateTime dataOraEntrata, DateTime? dataOraUscita)
+        {
+            this.dataOraEntrata = dataOraEntrata;
+            this.dataOraUscita = dataOraUscita;
+            this.targa = targa;
+            this.entrata = entrata;
+            this.uscita = uscita;
+        }*/
+
+    }
+}
